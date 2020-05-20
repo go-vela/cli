ff652afbdfaf22b85116c995a8282294196aba8f
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-05-20 11:33:09 -0500

    feat(internal): add Default output option

---
+++ internal/output/default_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package output
+
+import (
+	"testing"
+)
+
+func TestOutput_Default(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		failure bool
+		input   interface{}
+	}{
+		{
+			failure: false,
+			input:   "hello",
+		},
+		{ // map
+			failure: false,
+			input: map[string]string{"hello": "world"},
+		},
+		{ // slice
+			failure: false,
+			input: []interface{}{1, 2, 3},
+		},
+		{ // slice complex
+			failure: false,
+			input: []interface{}{struct{ Foo string }{Foo: "bar"}},
+		},
+		{ // complex
+			failure: false,
+			input: []struct{ Foo string }{{"bar"}, {"baz"}},
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		err := Default(test.input)
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("Default should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("Default returned err: %v", err)
+		}
+	}
+}

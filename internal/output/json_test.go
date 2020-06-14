2dd925c09b4ca4db5e37afa581ec77878f1d4170
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-06-14 15:31:07 -0500

    refactor: new file for rawjson

--- internal/output/json_test.go
+++ internal/output/json_test.go
@@ -67,48 +67,6 @@ func TestOutput_JSON(t *testing.T) {
 	}
 }
 
-func TestOutput_RawJSON(t *testing.T) {
-	// setup tests
-	tests := []struct {
-		failure bool
-		input   interface{}
-	}{
-		{
-			failure: false,
-			input:   "hello",
-		},
-		{
-			failure: true,
-			input:   new(failMarshaler),
-		},
-		{
-			failure: true,
-			input:   nil,
-		},
-		{
-			failure: true,
-			input:   "",
-		},
-	}
-
-	// run tests
-	for _, test := range tests {
-		err := RawJSON(test.input)
-
-		if test.failure {
-			if err == nil {
-				t.Errorf("RawJSON should have returned err")
-			}
-
-			continue
-		}
-
-		if err != nil {
-			t.Errorf("RawJSON returned err: %v", err)
-		}
-	}
-}
-
 type failMarshaler struct{}
 
 func (f *failMarshaler) MarshalJSON() ([]byte, error) {

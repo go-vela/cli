9d9060b48abcaf6f95276965cb5804aaad02b8dd
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-06-14 15:31:54 -0500

    feat: add output drivers

---
+++ internal/output/driver.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package output
+
+// output drivers
+const (
+	// DriverDefault defines the driver type
+	// when outputting in default format.
+	DriverDefault = "default"
+
+	// DriverDump defines the driver type
+	// when outputting in dump format.
+	DriverDump = "dump"
+
+	// DriverJSON defines the driver type
+	// when outputting in JSON format.
+	DriverJSON = "json"
+
+	// DriverRawJSON defines the driver type
+	// when outputting in raw JSON format.
+	DriverRawJSON = "rawjson"
+
+	// DriverSpew defines the driver type
+	// when outputting in github.com/davecgh/go-spew/spew format.
+	DriverSpew = "spew"
+
+	// DriverYAML defines the driver type
+	// when outputting in YAML format.
+	DriverYAML = "yaml"
+)

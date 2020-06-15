7a3c079ed460c65b1fe64d25ee694d864e2e4daa
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-06-15 15:19:18 -0500

    feat: add hook action

---
+++ action/hook/hook.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package hook
+
+// Config represents the configuration necessary
+// to perform hook related quests with Vela.
+type Config struct {
+	Action  string
+	Org     string
+	Repo    string
+	Number  int
+	Page    int
+	PerPage int
+	Output  string
+}

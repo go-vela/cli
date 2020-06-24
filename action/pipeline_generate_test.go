c4b7b6e7cd16e9757343e6ed718be295d1ac4526
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-06-24 07:45:52 -0500

    feat: add pipeline commands

---
+++ action/pipeline_generate_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package action
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/urfave/cli/v2"
+)
+
+func TestAction_PipelineGenerate(t *testing.T) {
+	// setup flags
+	set := flag.NewFlagSet("test", 0)
+	set.String("file", "generate.yml", "doc")
+	set.String("path", "pipeline/testdata", "doc")
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		set     *flag.FlagSet
+	}{
+		{
+			failure: false,
+			set:     set,
+		},
+		{
+			failure: true,
+			set:     flag.NewFlagSet("test", 0),
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		err := pipelineGenerate(cli.NewContext(nil, test.set, nil))
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("pipelineGenerate should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("pipelineGenerate returned err: %v", err)
+		}
+	}
+}

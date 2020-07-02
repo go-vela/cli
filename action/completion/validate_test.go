7472fae239d542b7f18e5397a272c7398ee0b359
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-07-02 10:42:04 -0500

    feat: add completion action

---
+++ action/completion/validate_test.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package completion
+
+import (
+	"testing"
+)
+
+func TestCompletion_Config_Validate(t *testing.T) {
+	// setup tests
+	tests := []struct {
+		failure bool
+		config  *Config
+	}{
+		{
+			failure: false,
+			config: &Config{
+				Action: "generate",
+				Bash:   true,
+			},
+		},
+		{
+			failure: false,
+			config: &Config{
+				Action: "generate",
+				Zsh:    true,
+			},
+		},
+		{
+			failure: true,
+			config: &Config{
+				Action: "generate",
+				Bash:   true,
+				Zsh:    true,
+			},
+		},
+		{
+			failure: true,
+			config: &Config{
+				Action: "generate",
+			},
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		err := test.config.Validate()
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("Validate should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("Validate returned err: %v", err)
+		}
+	}
+}

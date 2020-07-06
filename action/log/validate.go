fe7ba21ba64a1256a58f32a6a046ceb780788cc6
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-07-06 13:01:44 -0500

    chore(action): add log logging statements

--- action/log/validate.go
+++ action/log/validate.go
@@ -6,10 +6,14 @@ package log
 
 import (
 	"fmt"
+
+	"github.com/sirupsen/logrus"
 )
 
 // Validate verifies the configuration provided.
 func (c *Config) Validate() error {
+	logrus.Debug("validating log configuration")
+
 	// check if log org is set
 	if len(c.Org) == 0 {
 		return fmt.Errorf("no log org provided")

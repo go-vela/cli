ea333686d0df763ee050f8034b4b71f4b3ef775a
Jordan Brockopp <Jordan.Brockopp@target.com>
2020-08-03 08:45:24 -0500

    fix(action): set log level after loading config

--- action/load.go
+++ action/load.go
@@ -7,6 +7,8 @@ package action
 import (
 	"github.com/go-vela/cli/action/config"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/urfave/cli/v2"
 )
 
@@ -32,6 +34,26 @@ func load(c *cli.Context) error {
 		if err != nil {
 			return err
 		}
+	}
+
+	// set log level for the CLI
+	switch c.String("log.level") {
+	case "t", "trace", "Trace", "TRACE":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "d", "debug", "Debug", "DEBUG":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "w", "warn", "Warn", "WARN":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "e", "error", "Error", "ERROR":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "f", "fatal", "Fatal", "FATAL":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "p", "panic", "Panic", "PANIC":
+		logrus.SetLevel(logrus.PanicLevel)
+	case "i", "info", "Info", "INFO":
+		fallthrough
+	default:
+		logrus.SetLevel(logrus.InfoLevel)
 	}
 
 	return nil

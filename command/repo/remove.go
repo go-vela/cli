91de97b93ec93481b7f5bf410edd598f3fb21b7f
Jordan <Jordan.Brockopp@target.com>
2021-10-27 08:38:09 -0500

    feat: add repo commands and package

---
+++ command/repo/remove.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package repo
+
+import (
+	"fmt"
+
+	"github.com/go-vela/cli/action"
+	"github.com/go-vela/cli/action/repo"
+	"github.com/go-vela/cli/internal"
+	"github.com/go-vela/cli/internal/client"
+
+	"github.com/urfave/cli/v2"
+)
+
+// CommandRemove defines the command for removing a repository.
+var CommandRemove = &cli.Command{
+	Name:        "repo",
+	Description: "Use this command to remove a repository.",
+	Usage:       "Remove the provided repository",
+	Action:      remove,
+	Flags: []cli.Flag{
+
+		// Repo Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_ORG", "REPO_ORG"},
+			Name:    internal.FlagOrg,
+			Aliases: []string{"o"},
+			Usage:   "provide the organization for the repository",
+		},
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_REPO", "REPO_NAME"},
+			Name:    internal.FlagRepo,
+			Aliases: []string{"r"},
+			Usage:   "provide the name for the repository",
+		},
+
+		// Output Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_OUTPUT", "REPO_OUTPUT"},
+			Name:    internal.FlagOutput,
+			Aliases: []string{"op"},
+			Usage:   "format the output in json, spew or yaml",
+		},
+	},
+	CustomHelpTemplate: fmt.Sprintf(`%s
+EXAMPLES:
+  1. Remove a repository.
+    $ {{.HelpName}} --org MyOrg --repo MyRepo
+  2. Remove a repository with json output.
+    $ {{.HelpName}} --org MyOrg --repo MyRepo --output json
+  3. Remove a repository when config or environment variables are set.
+    $ {{.HelpName}}
+
+DOCUMENTATION:
+
+  https://go-vela.github.io/docs/reference/cli/repo/remove/
+`, cli.CommandHelpTemplate),
+}
+
+// helper function to capture the provided input
+// and create the object used to remove a repository.
+func remove(c *cli.Context) error {
+	// load variables from the config file
+	err := action.Load(c)
+	if err != nil {
+		return err
+	}
+
+	// parse the Vela client from the context
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/internal/client?tab=doc#Parse
+	client, err := client.Parse(c)
+	if err != nil {
+		return err
+	}
+
+	// create the repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config
+	r := &repo.Config{
+		Action: internal.ActionRemove,
+		Org:    c.String(internal.FlagOrg),
+		Name:   c.String(internal.FlagRepo),
+		Output: c.String(internal.FlagOutput),
+	}
+
+	// validate repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config.Validate
+	err = r.Validate()
+	if err != nil {
+		return err
+	}
+
+	// execute the remove call for the repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config.Remove
+	return r.Remove(client)
+}
---
+++ command/repo/view.go
@@ -0,0 +1,105 @@
+// Copyright (c) 2021 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package repo
+
+import (
+	"fmt"
+
+	"github.com/go-vela/cli/action"
+	"github.com/go-vela/cli/action/repo"
+	"github.com/go-vela/cli/internal"
+	"github.com/go-vela/cli/internal/client"
+
+	"github.com/urfave/cli/v2"
+)
+
+// CommandView defines the command for inspecting a repository.
+var CommandView = &cli.Command{
+	Name:        "repo",
+	Description: "Use this command to view a repo.",
+	Usage:       "View details of the provided repo",
+	Action:      view,
+	Flags: []cli.Flag{
+
+		// Repo Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_ORG", "REPO_ORG"},
+			Name:    internal.FlagOrg,
+			Aliases: []string{"o"},
+			Usage:   "provide the organization for the repository",
+		},
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_REPO", "REPO_NAME"},
+			Name:    internal.FlagRepo,
+			Aliases: []string{"r"},
+			Usage:   "provide the name for the repository",
+		},
+
+		// Output Flags
+
+		&cli.StringFlag{
+			EnvVars: []string{"VELA_OUTPUT", "REPO_OUTPUT"},
+			Name:    internal.FlagOutput,
+			Aliases: []string{"op"},
+			Usage:   "format the output in json, spew or yaml",
+			Value:   "yaml",
+		},
+	},
+	CustomHelpTemplate: fmt.Sprintf(`%s
+EXAMPLES:
+  1. View details of a repository.
+    $ {{.HelpName}} --org MyOrg --repo MyRepo
+  2. View details of a repository with json output.
+    $ {{.HelpName}} --org MyOrg --repo MyRepo --output json
+  3. View details of a repository when config or environment variables are set.
+    $ {{.HelpName}}
+
+DOCUMENTATION:
+
+  https://go-vela.github.io/docs/reference/cli/repo/view/
+`, cli.CommandHelpTemplate),
+}
+
+// helper function to capture the provided input
+// and create the object used to inspect a repository.
+func view(c *cli.Context) error {
+	// load variables from the config file
+	err := action.Load(c)
+	if err != nil {
+		return err
+	}
+
+	// parse the Vela client from the context
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/internal/client?tab=doc#Parse
+	client, err := client.Parse(c)
+	if err != nil {
+		return err
+	}
+
+	// create the repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config
+	r := &repo.Config{
+		Action: internal.ActionView,
+		Org:    c.String(internal.FlagOrg),
+		Name:   c.String(internal.FlagRepo),
+		Output: c.String(internal.FlagOutput),
+	}
+
+	// validate repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config.Validate
+	err = r.Validate()
+	if err != nil {
+		return err
+	}
+
+	// execute the view call for the repo configuration
+	//
+	// https://pkg.go.dev/github.com/go-vela/cli/action/repo?tab=doc#Config.View
+	return r.View(client)
+}

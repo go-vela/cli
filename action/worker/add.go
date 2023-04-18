3bad9799e1b4401f5b9a4bc626f9cbbc4b347314
David May <49894298+wass3rw3rk@users.noreply.github.com>
2023-04-18 16:09:32 -0500

    fix: worker registration (#438)

--- action/worker/add.go
+++ action/worker/add.go
@@ -5,7 +5,12 @@
 package worker
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/go-vela/cli/internal/output"
 
@@ -28,13 +33,49 @@ func (c *Config) Add(client *vela.Client) error {
 
 	logrus.Tracef("got registration token, adding worker %q", c.Hostname)
 
-	// send API call to register a worker
+	// create a custom http client using the registration token received
+	// from .RegisterToken as a Bearer token.
 	//
-	// https://pkg.go.dev/github.com/go-vela/sdk-go/vela#AdminWorkerService.Register
-	msg, _, err := client.Admin.Worker.Register(c.Address, registerToken.GetToken())
+	// we will make a call to the registration endpoint on the worker
+	// at the given c.Address for the worker.
+	workerRegistrationURL := strings.TrimSuffix(c.Address, "/")
+	workerRegistrationURL = fmt.Sprintf("%s/register", workerRegistrationURL)
+
+	// create a new request for the given URL (c.Address)
+	req, err := http.NewRequestWithContext(context.Background(), "POST", workerRegistrationURL, nil)
 	if err != nil {
-		return fmt.Errorf("unable to register worker at %q: %w", c.Address, err)
+		return fmt.Errorf("unable to form request for worker registration endpoint")
 	}
+
+	// add the authorization header using the registration token
+	req.Header.Add("Authorization", "Bearer "+registerToken.GetToken())
+
+	// add the user agent for the request
+	req.Header.Add("User-Agent", client.UserAgent)
+
+	// create a new http client
+	httpClient := http.DefaultClient
+	httpClient.Timeout = time.Second * 15
+
+	// perform the request to the worker registration endpoint
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error on response to worker registration endpoint")
+	}
+	defer resp.Body.Close()
+
+	// read the body response
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read response body for worker registration call")
+	}
+
+	// if the call was successful but didn't register successfully
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error while registering worker at %q, received: %d", workerRegistrationURL, resp.StatusCode)
+	}
+
+	out := fmt.Sprintf("worker %q registered successfully", c.Hostname)
 
 	logrus.Tracef("worker %q registered", c.Hostname)
 
@@ -44,26 +85,26 @@ func (c *Config) Add(client *vela.Client) error {
 		// output the worker in dump format
 		//
 		// https://pkg.go.dev/github.com/go-vela/cli/internal/output?tab=doc#Dump
-		return output.Dump(msg)
+		return output.Dump(out)
 	case output.DriverJSON:
 		// output the worker in JSON format
 		//
 		// https://pkg.go.dev/github.com/go-vela/cli/internal/output?tab=doc#JSON
-		return output.JSON(msg)
+		return output.JSON(out)
 	case output.DriverSpew:
 		// output the worker in spew format
 		//
 		// https://pkg.go.dev/github.com/go-vela/cli/internal/output?tab=doc#Spew
-		return output.Spew(msg)
+		return output.Spew(out)
 	case output.DriverYAML:
 		// output the worker in YAML format
 		//
 		// https://pkg.go.dev/github.com/go-vela/cli/internal/output?tab=doc#YAML
-		return output.YAML(msg)
+		return output.YAML(out)
 	default:
 		// output the worker in stdout format
 		//
 		// https://pkg.go.dev/github.com/go-vela/cli/internal/output?tab=doc#Stdout
-		return output.Stdout(*msg)
+		return output.Stdout(out)
 	}
 }

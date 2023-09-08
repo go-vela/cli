c035f52648feb05355ee0319313bd457cff0fcc9
TimHuynh <tim_huynh94@yahoo.com>
2023-09-08 15:04:47 -0500

    working test

--- action/worker/add.go
+++ action/worker/add.go
@@ -5,10 +5,15 @@
 package worker
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/go-vela/cli/internal/output"
+	"io"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/go-vela/sdk-go/vela"
 
@@ -28,8 +33,13 @@ func (c *Config) Add(client *vela.Client) error {
 
 	logrus.Tracef("got worker registration details, adding worker %q", c.Hostname)
 
-	// create a custom http client using the registration token received
-	// from .RegisterToken as a Bearer token.
+	// convert WorkerRegistration struct into bytes for request body
+	b, err := json.Marshal(wr)
+	if err != nil {
+		return fmt.Errorf("unable to marshal worker registration details: %w", err)
+	}
+	body := bytes.NewReader(b)
+	// create a custom http client
 	//
 	// we will make a call to the registration endpoint on the worker
 	// at the given c.Address for the worker.
@@ -37,39 +47,35 @@ func (c *Config) Add(client *vela.Client) error {
 	workerRegistrationURL = fmt.Sprintf("%s/register", workerRegistrationURL)
 
 	// send request using client
-	resp, err := client.Call("POST", workerRegistrationURL, wr, nil)
 	// create a new request for the given URL (c.Address)
-	//req, err := http.NewRequestWithContext(context.Background(), "POST", workerRegistrationURL, nil)
+	req, err := http.NewRequestWithContext(context.Background(), "POST", workerRegistrationURL, body)
 	if err != nil {
 		return fmt.Errorf("unable to form request for worker registration endpoint")
 	}
 
-	// add the authorization header using the registration token
-	//req.Header.Add("Authorization", "Bearer "+registerToken.GetToken())
-	//
-	//// add the user agent for the request
-	//req.Header.Add("User-Agent", client.UserAgent)
+	// add the user agent for the request
+	req.Header.Add("User-Agent", client.UserAgent)
 
-	// create a new http client
-	//httpClient := http.DefaultClient
-	//httpClient.Timeout = time.Second * 15
-	//
-	//// perform the request to the worker registration endpoint
-	//resp, err := httpClient.Do(req)
-	//if err != nil {
-	//	return fmt.Errorf("error on response to worker registration endpoint")
-	//}
-	//defer resp.Body.Close()
+	//create a new http client
+	httpClient := http.DefaultClient
+	httpClient.Timeout = time.Second * 15
+
+	// perform the request to the worker registration endpoint
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error on response to worker registration endpoint")
+	}
+	defer resp.Body.Close()
 
 	// read the body response
-	//bodyBytes, err := io.ReadAll(resp.Body)
-	//if err != nil {
-	//	return fmt.Errorf("unable to read response body for worker registration call %v", resp.StatusCode)
-	//}
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read response body for worker registration call %v", resp.StatusCode)
+	}
 
 	// if the call was successful but didn't register successfully
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("error while registering worker at %q, received: %d - %s", workerRegistrationURL, resp.StatusCode, resp.Body.Close())
+		return fmt.Errorf("error while registering worker at %q, received: %d - %s", workerRegistrationURL, resp.StatusCode, string(bodyBytes))
 	}
 
 	out := fmt.Sprintf("worker %q registered successfully", c.Hostname)

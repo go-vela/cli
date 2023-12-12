cf6aee3e463137e26e63c666c8c407f685ae758c
Easton Crupper <65553218+ecrupper@users.noreply.github.com>
2023-12-12 10:03:10 -0500

    feat(build): add approve command (#508)
    * feat(build): add approve command
    
    * remove alias for approve

---
+++ command/build/approve_test.go
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package build
+
+import (
+	"flag"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-vela/cli/test"
+	"github.com/go-vela/server/mock/server"
+
+	"github.com/urfave/cli/v2"
+)
+
+func TestBuild_Approve(t *testing.T) {
+	// setup test server
+	s := httptest.NewServer(server.FakeHandler())
+
+	// setup flags
+	authSet := flag.NewFlagSet("test", 0)
+	authSet.String("api.addr", s.URL, "doc")
+	authSet.String("api.token.access", test.TestTokenGood, "doc")
+	authSet.String("api.token.refresh", "superSecretRefreshToken", "doc")
+
+	fullSet := flag.NewFlagSet("test", 0)
+	fullSet.String("api.addr", s.URL, "doc")
+	fullSet.String("api.token.access", test.TestTokenGood, "doc")
+	fullSet.String("api.token.refresh", "superSecretRefreshToken", "doc")
+	fullSet.String("org", "github", "doc")
+	fullSet.String("repo", "octocat", "doc")
+	fullSet.Int("build", 1, "doc")
+
+	// setup tests
+	tests := []struct {
+		failure bool
+		set     *flag.FlagSet
+	}{
+		{
+			failure: false,
+			set:     fullSet,
+		},
+		{
+			failure: true,
+			set:     authSet,
+		},
+		{
+			failure: true,
+			set:     flag.NewFlagSet("test", 0),
+		},
+	}
+
+	// run tests
+	for _, test := range tests {
+		err := approve(cli.NewContext(&cli.App{Name: "vela", Version: "v0.0.0"}, test.set, nil))
+
+		if test.failure {
+			if err == nil {
+				t.Errorf("approve should have returned err")
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("approve returned err: %v", err)
+		}
+	}
+}

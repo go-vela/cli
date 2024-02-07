f91caa7fb4c5bc918131737cf0a4909dbe362c1b
ecrupper <easton.crupper12@gmail.com>
2024-02-07 10:12:14 -0600

    fix(deployments): support commas in values for deploy parameters

--- action/deployment/add_test.go
+++ action/deployment/add_test.go
@@ -4,7 +4,6 @@ package deployment
 
 import (
 	"net/http/httptest"
-	"reflect"
 	"testing"
 
 	"github.com/go-vela/sdk-go/vela"
@@ -90,21 +89,10 @@ func TestDeployment_Config_Add(t *testing.T) {
 				Target:      "production",
 				Task:        "deploy:vela",
 				Output:      "yaml",
-				Parameters:  []string{"foo=test1", "bar=test2"},
-			},
-		},
-		{
-			failure: true,
-			config: &Config{
-				Action:      "add",
-				Org:         "github",
-				Repo:        "octocat",
-				Description: "Deployment request from Vela",
-				Ref:         "refs/heads/main",
-				Target:      "production",
-				Task:        "deploy:vela",
-				Output:      "yaml",
-				Parameters:  []string{"badinput"},
+				Parameters: raw.StringSliceMap{
+					"foo": "bar",
+					"faz": "baz",
+				},
 			},
 		},
 	}
@@ -124,35 +112,5 @@ func TestDeployment_Config_Add(t *testing.T) {
 		if err != nil {
 			t.Errorf("Add returned err: %v", err)
 		}
-	}
-}
-
-func Test_parseKeyValue(t *testing.T) {
-	type args struct {
-		input []string
-	}
-
-	tests := []struct {
-		name    string
-		args    args
-		want    raw.StringSliceMap
-		wantErr bool
-	}{
-		{"valid input", args{input: []string{"foo=test1", "bar=test2"}}, raw.StringSliceMap{"foo": "test1", "bar": "test2"}, false},
-		{"invalid input", args{input: []string{"foo=test1", "badinput"}}, nil, true},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseKeyValue(tt.args.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("parseKeyValue() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("parseKeyValue() = %v, want %v", got, tt.want)
-			}
-		})
 	}
 }

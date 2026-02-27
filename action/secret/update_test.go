9d545876ac724db66ab3e6139c9a1f79fad90964
wass3r <1301201+wass3r@users.noreply.github.com>
2026-02-27 14:10:51 -0600

    add failing test
    images, repo sets should not be sent if not supplied

--- action/secret/update_test.go
+++ action/secret/update_test.go
@@ -3,10 +3,14 @@
 package secret
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/go-vela/sdk-go/vela"
+	api "github.com/go-vela/server/api/types"
 	"github.com/go-vela/server/mock/server"
 )
 
@@ -245,5 +249,73 @@ func TestSecret_Config_UpdateFromFile(t *testing.T) {
 		if err != nil {
 			t.Errorf("UpdateFromFile returned err: %v", err)
 		}
+	}
+}
+
+func TestSecret_Config_Update_NoImageOrRepoAllowlist(t *testing.T) {
+	// setup test server
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			t.Errorf("expected PUT request, got %s", r.Method)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("unable to read request body: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		payload := map[string]json.RawMessage{}
+		err = json.Unmarshal(body, &payload)
+		if err != nil {
+			t.Errorf("unable to decode request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if _, ok := payload["images"]; ok {
+			t.Errorf("expected images to be omitted in update payload")
+		}
+
+		if _, ok := payload["repo_allowlist"]; ok {
+			t.Errorf("expected repo_allowlist to be omitted in update payload")
+		}
+
+		secret := &api.Secret{}
+		secret.SetOrg("github")
+		secret.SetRepo("octocat")
+		secret.SetName("foo")
+		secret.SetType("repo")
+		secret.SetValue("bar")
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(w).Encode(secret)
+	}))
+	defer s.Close()
+
+	// create a vela client
+	client, err := vela.NewClient(s.URL, "vela", nil)
+	if err != nil {
+		t.Errorf("unable to create client: %v", err)
+	}
+
+	config := &Config{
+		Action: "update",
+		Engine: "native",
+		Type:   "repo",
+		Org:    "github",
+		Repo:   "octocat",
+		Name:   "foo",
+		Value:  "bar",
+		Output: "",
+	}
+
+	err = config.Update(t.Context(), client)
+	if err != nil {
+		t.Errorf("Update returned err: %v", err)
 	}
 }
